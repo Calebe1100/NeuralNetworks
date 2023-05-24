8fb9585fc462907b1ae544b3f0842509fa8ab32b
Calebe Libério <calebe.liberio@vertown.com>
2023-05-24 13:42:10 -0300

    Adicionar arquivos de projeto.

---
+++ Controller.cs
@@ -0,0 +1,41 @@
+﻿using NeuralNetworks.useCases;
+using System;
+
+namespace NeuralNetworks
+{
+    public static class Controller
+    {
+        public static double[][][] Base { get; set; } =
+            new double[][][] {
+               new double[][] { new double[] { 0, 0 }, new double[] { 0 } },
+                new double[][]{ new double[]  { 0 ,1 }, new double[] { 1 } },
+                new double[][]{ new double[] { 1 ,0 }, new double[] { 1 } },
+                new double[][]{ new double[] { 1 ,1 }, new double[] { 0 } }
+            };
+        public static void Main()
+        {
+
+            Perceptron perceptron = new Perceptron(2, 1, 0.3);
+
+            for (int e = 0; e < 1000; e++)
+            {
+                double periodError = 0;
+                for (int a = 0; a < Base.Length; a++)
+                {
+                    double sampleError = 0;
+                    double[] inputX = Base[a][0];
+                    double[] inputY = Base[a][1];
+
+                    double[] theta = perceptron.TrainnerExecute(inputX, inputY);
+
+                    for (int i = 0; i < inputY.Length; i++)
+                    {
+                        sampleError = Math.Abs(inputY[i] - theta[i]);
+                    periodError += sampleError;
+                }
+                    }
+                    Console.WriteLine("Epoca: " + e + " - erro:" + periodError);
+            }
+        }
+    }
+}
---
+++ useCases/Perceptron.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks.useCases
+{
+    public class Perceptron
+    {
+        public int AmountIn { get; set; }
+        public int AmountOut { get; set; }
+        public double[,] Networks { get; set; }
+        public double Ni { get; set; }
+
+        public Random Random { get; set; } = new Random();
+        public Perceptron(int amountIn, int amoutOut, double ni)
+        {
+            this.AmountIn = amountIn;
+            this.AmountOut = amoutOut;
+            this.Ni = ni;
+            Networks = new double[amountIn+1,amoutOut +1];
+            Networks.Initialize();
+
+            this.FillValues();
+
+        }
+
+        public double[] TrainnerExecute(double[] input, double[] values)
+        {
+            double[] formatInput = input.Concat(Enumerable.Repeat(1.0, 1)).ToArray();
+            double[] theta = new double[values.Length];
+
+            for (int j = 0; j < values.Length; j++)
+            {
+                double uj = 0;
+                for (int i = 0; i < formatInput.Length; i++)
+                {
+                    uj += formatInput[i] * Networks[i,j];
+                }
+                theta[j] = (1 / (1 + Math.Exp(-uj)));
+            }
+
+            for (int j = 0; j < values.Length; j++)
+            {
+                for (int i = 0; i < formatInput.Length; i++)
+                {
+                    Networks[i,j] += Ni * (values[j] - theta[j]) * formatInput[i];
+                }
+            }
+            return theta;
+        }
+
+        private void FillValues()
+        {
+            double max = 0.03;
+            double min = -0.03;
+
+            for (int i = 0; i <= this.AmountIn; i++)
+            {
+                for (int j = 0; j <= this.AmountOut; j++)
+                {
+                    this.Networks[i,j] = min + (max - min) * Random.NextDouble();
+                }
+            }
+        }
+
+    }
+}
